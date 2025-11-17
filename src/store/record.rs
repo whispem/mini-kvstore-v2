d505c369a1589849994f14c9f1f26870fe1d93e0
Em <contact.whispem@gmail.com>
2025-11-17 14:47:54 +0100

    Refactor Record and RecordHeader structures

--- src/store/record.rs
+++ src/store/record.rs
@@ -1,3 +1,58 @@
+use std::io::{Read, Write, Result};
+
+pub const TOMBSTONE_MARKER: u32 = u32::MAX;
+
+#[derive(Debug, Clone)]
+pub struct RecordHeader {
+    pub key_len: u32,
+    pub value_len: u32,
+    pub flags: u8,
+    pub checksum: u32,
+}
+
+impl RecordHeader {
+    pub fn new(key_len: u32, value_len: u32, flags: u8, checksum: u32) -> Self {
+        Self {
+            key_len,
+            value_len,
+            flags,
+            checksum,
+        }
+    }
+    
+    pub const HEADER_SIZE: usize = 13;
+    
+    pub fn write_to<W: Write>(&self, writer: &mut W) -> Result<()> {
+        writer.write_all(&self.key_len.to_le_bytes())?;
+        writer.write_all(&self.value_len.to_le_bytes())?;
+        writer.write_all(&[self.flags])?;
+        writer.write_all(&self.checksum.to_le_bytes())?;
+        Ok(())
+    }
+    
+    pub fn read_from<R: Read>(reader: &mut R) -> Result<Option<Self>> {
+        let mut buf = [0u8; Self::HEADER_SIZE];
+        
+        // Try to read header, return None if EOF
+        match reader.read_exact(&mut buf) {
+            Ok(_) => {
+                let key_len = u32::from_le_bytes([buf[0], buf[1], buf[2], buf[3]]);
+                let value_len = u32::from_le_bytes([buf[4], buf[5], buf[6], buf[7]]);
+                let flags = buf[8];
+                let checksum = u32::from_le_bytes([buf[9], buf[10], buf[11], buf[12]]);
+                
+                Ok(Some(Self {
+                    key_len,
+                    value_len,
+                    flags,
+                    checksum,
+                }))
+            }
+            Err(_) => Ok(None)
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct Record {
     pub key: Vec<u8>,
@@ -7,10 +62,25 @@ pub struct Record {
 
 impl Record {
     pub fn new(key: Vec<u8>, value: Vec<u8>) -> Self {
-        Self { key, value, deleted: false }
+        Self { 
+            key, 
+            value, 
+            deleted: false 
+        }
     }
-
+    
     pub fn tombstone(key: Vec<u8>) -> Self {
-        Self { key, value: Vec::new(), deleted: true }
+        Self { 
+            key, 
+            value: Vec::new(), 
+            deleted: true 
+        }
     }
+}
+
+pub fn compute_checksum(key: &[u8], value: &[u8]) -> u32 {
+    let mut hasher = crc32fast::Hasher::new();
+    hasher.update(key);
+    hasher.update(value);
+    hasher.finalize()
 }
