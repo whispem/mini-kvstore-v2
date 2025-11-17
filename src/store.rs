833a936721e7bbfc16984d581265894e11e76fc9
Em <contact.whispem@gmail.com>
2025-11-17 15:33:04 +0100

    Refactor module declarations in store.rs

--- src/store.rs
+++ src/store.rs
@@ -1,11 +1,6 @@
-pub mod compaction;
-pub mod config;
-pub mod engine;
-pub mod index;
-pub mod record;
-pub mod segment;
-pub mod stats;
-
-pub use config::{FsyncPolicy, StoreConfig};
-pub use engine::KVStore;
-pub use stats::StoreStats;
+mod config;
+mod engine;
+mod index;
+mod record;
+mod segment;
+mod stats;
