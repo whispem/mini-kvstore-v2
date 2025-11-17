6f8072e9030de92ff8b71a905a1b341da9a440dc
Em <contact.whispem@gmail.com>
2025-11-17 13:45:33 +0100

    Refactor StoreConfig to include new constructor

--- src/store/config.rs
+++ src/store/config.rs
@@ -1,7 +1,8 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 #[derive(Debug, Clone)]
 pub enum FsyncPolicy {
+
     Always,
     Never,
     Batch,
@@ -16,10 +17,18 @@ pub struct StoreConfig {
 }
 
 impl StoreConfig {
+
+    pub fn new(data_dir: impl AsRef<Path>) -> Self {
+        Self {
+            data_dir: data_dir.as_ref().to_path_buf(),
+            ..Self::default()
+        }
+    }
+
     pub fn default() -> Self {
         StoreConfig {
             data_dir: PathBuf::from("data"),
-            segment_size: 1024 * 1024,
+            segment_size: 1024 * 1024, 
             fsync: FsyncPolicy::Always,
             compaction_threshold: 3,
         }
