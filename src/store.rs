25fb8238ceebe2f16b763650549e56a194f8a241
Em <contact.whispem@gmail.com>
2025-11-21 15:08:38 +0100

    Reorder and add modules in store.rs

--- src/store.rs
+++ src/store.rs
@@ -1,11 +1,11 @@
 //! Store module root.
 
-pub mod engine;
 pub mod compaction;
 pub mod config;
+pub mod engine;
+pub mod error;
 pub mod index;
 pub mod segment;
-pub mod error;
 pub mod stats;
 
 // Useful public re-export for ease of use
