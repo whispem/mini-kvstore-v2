c4de080b9f911028fb727374644e1a967738671b
Em <contact.whispem@gmail.com>
2025-11-21 19:43:41 +0100

    Refactor store module by removing comments

--- src/store.rs
+++ src/store.rs
@@ -1,5 +1,3 @@
-//! Store module root.
-
 pub mod compaction;
 pub mod config;
 pub mod engine;
@@ -8,5 +6,4 @@ pub mod index;
 pub mod segment;
 pub mod stats;
 
-// Useful public re-export for ease of use
 pub use engine::KVStore;
