87bba36ea19e67d940224e780ec6b167576f0dde
Em <contact.whispem@gmail.com>
2025-11-21 20:23:36 +0100

    Refactor error handling with StoreError enum

--- src/store/error.rs
+++ src/store/error.rs
@@ -1,2 +1,15 @@
-// mini-kvstore-v2/src/store/error.rs
-pub type Result<T> = std::result::Result<T, std::io::Error>;
+use std::io;
+
+#[derive(thiserror::Error, Debug)]
+pub enum StoreError {
+    #[error("IO error: {0}")]
+    Io(#[from] io::Error),
+
+    #[error("Key not found")]
+    KeyNotFound,
+
+    #[error("Corrupted data")]
+    CorruptedData,
+}
+
+pub type Result<T> = std::result::Result<T, StoreError>;
