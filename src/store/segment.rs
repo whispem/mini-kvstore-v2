7c82529b12c2b4e3d4e0711dae34817f50c1cefc
Em <contact.whispem@gmail.com>
2025-11-21 20:38:09 +0100

    Refactor SegmentReadResult and open method
    Refactor segment.rs to improve type handling and remove Clippy warnings.

--- src/store/segment.rs
+++ src/store/segment.rs
@@ -1,35 +1,32 @@
 //! Segment logic for mini-kvstore-v2.
-// Clippy warnings silenced (including type complexity).
 
-pub type SegmentReadResult = Result<Option<(String, Option<Vec<u8>>)>, std::io::Error>;
+use crate::store::error::{Result, StoreError};
 
-#[allow(dead_code)]
+pub type SegmentReadResult = Result<Option<(String, Option<Vec<u8>>)>>;
+
 const SEGMENT_SIZE_LIMIT: u64 = 1024 * 1024;
 
-#[allow(dead_code)]
 pub struct Segment {
     pub path: std::path::PathBuf,
-    pub id: usize, // Unused field allowed for Clippy
-                   // TODO: Add other fields if necessary.
+    pub id: usize,
 }
 
-#[allow(dead_code)]
 impl Segment {
     /// Opens a segment (stub implementation).
-    pub fn open(_dir: &std::path::Path, _id: usize) -> Result<Self, std::io::Error> {
+    pub fn open(dir: &std::path::Path, id: usize) -> Result<Self> {
         Ok(Segment {
-            path: _dir.join(format!("segment-{:04}.dat", _id)),
-            id: _id,
+            path: dir.join(format!("segment-{:04}.dat", id)),
+            id,
         })
     }
 
     /// Appends a key-value pair to the segment (stub).
-    pub fn append(&mut self, _key: &[u8], _value: &[u8]) -> Result<u64, std::io::Error> {
+    pub fn append(&mut self, _key: &[u8], _value: &[u8]) -> Result<u64> {
         Ok(0)
     }
 
     /// Appends a tombstone (delete marker) for a key.
-    pub fn append_tombstone(&mut self, _key: &[u8]) -> Result<u64, std::io::Error> {
+    pub fn append_tombstone(&mut self, _key: &[u8]) -> Result<u64> {
         Ok(0)
     }
 
@@ -44,7 +41,7 @@ impl Segment {
     }
 
     /// Reads a value at a given offset (stub).
-    pub fn read_value_at(&mut self, _offset: u64) -> Result<Option<Vec<u8>>, std::io::Error> {
+    pub fn read_value_at(&mut self, _offset: u64) -> Result<Option<Vec<u8>>> {
         Ok(None)
     }
 
