ec1a88bf4644bb178a96d8580269e290dbb7bd74
Em <contact.whispem@gmail.com>
2025-11-21 19:06:27 +0100

    Refactor integration test for KVStore compaction

--- tests/store_integration.rs
+++ tests/store_integration.rs
@@ -1,15 +1,14 @@
-use mini_kvstore_v2::KVStore; 
+use mini_kvstore_v2::KVStore;
 mod common;
-use common::{setup_test_dir, cleanup_test_dir};
+use common::{cleanup_test_dir, setup_test_dir};
 
 #[test]
 fn compaction_after_many_updates() {
-    let test_dir = "tests_data/int_compaction_updates";
+    let test_dir = "test_compact_db";
     setup_test_dir(test_dir);
 
     let mut store = KVStore::open(test_dir).unwrap();
 
-    
     for round in 0..5 {
         for i in 0..100 {
             let key = format!("key_{}", i);
@@ -20,14 +19,12 @@ fn compaction_after_many_updates() {
 
     store.compact().unwrap();
 
-    
     for i in 0..100 {
         let key = format!("key_{}", i);
         let expected = format!("value_{}_4", i); // Last round value
-        assert_eq!(store.get(&key).unwrap(), Some(expected.as_bytes().to_vec()));
+        assert_eq!(store.get(&key), Some(expected.as_bytes().to_vec()));
     }
 
-   
     let stats = store.stats();
     assert_eq!(stats.num_keys, 100, "Should have exactly 100 keys after compaction");
 
